30c2dacd11d086985cdda889d32b9caa87fb6ff1
Archit Singh <contact.architsingh@gmail.com>
2023-11-06 17:49:38 +0530

    add start benchmark

--- audio_utils.py
+++ audio_utils.py
@@ -1,4 +1,16 @@
+import os
+
 from mutagen import File
+
+def get_audio_file_size_mb(audio_file_path):
+    audio_file_path = str(audio_file_path)
+    
+    if not os.path.exists(audio_file_path):
+        return None
+    
+    file_size_bytes = os.path.getsize(audio_file_path)
+    file_size_mb = file_size_bytes / (1024 * 1024)
+    return file_size_mb
 
 
 def get_audio_stats(file_path):
@@ -15,6 +27,11 @@ def get_audio_stats(file_path):
         formatted_length = "{:.2f}".format(audio_stats['length'])
         audio_stats['length'] = formatted_length
         audio_stats.pop('pprint')
+        
+        file_size_mb = get_audio_file_size_mb(file_path)
+        file_size_mb = "{:.2f}".format(file_size_mb) + " MB"
+        audio_stats['file_size_mb'] = file_size_mb
+        
         return audio_stats
 
     return None
--- vanilla_whisper_benchmark.py
+++ vanilla_whisper_benchmark.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from audio_utils import get_audio_stats
 
 import whisper
+import pandas as pd
+
 
 def get_wav_files_in_directory(directory_path):
     directory_path = Path(directory_path)
@@ -15,17 +17,21 @@ def get_wav_files_in_directory(directory_path):
     return wav_file_paths
 
 def get_transcription(audio_file_path, model="tiny"):
-    stats = get_audio_stats(audio_file_path)
     model = whisper.load_model(name=model, in_memory=True)
     result = model.transcribe(audio_file_path)
-    return result, stats
+    return result
 
 audio_directory_path = "/Users/architsingh/Documents/projects/asr-call-center/test_set/audio"
 wav_files = get_wav_files_in_directory(audio_directory_path)
 
 for audio_file_path in wav_files:
     audio_file_path = str(audio_file_path)
+    stats = get_audio_stats(audio_file_path)
+    print(stats)
     s = time.time()
-    get_transcription(audio_file_path)
+    # models = ["tiny", "base", "small", "medium", "large-v1", "large-v2"]
+    models = ["tiny"]
+    for model in models:
+        get_transcription(audio_file_path=audio_file_path, model=model)
     e = time.time()
     print(e-s)
