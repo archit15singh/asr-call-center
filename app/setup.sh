74f32c32edff009879b8d64d00045732b1b3c244
Archit Singh <contact.architsingh@gmail.com>
2023-11-11 05:43:32 +0530

    changes

---
+++ app/setup.sh
@@ -0,0 +1,14 @@
+mkdir -p data
+mkdir -p converted
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+brew install ffmpeg
+
+git clone https://github.com/ggerganov/whisper.cpp
+cd whisper.cpp
+bash ./models/generate-coreml-model.sh medium.en
+make clean
+WHISPER_COREML=1 make -j
